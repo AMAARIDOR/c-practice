b4144d84bfd6ff3fc682c9acb1063a6b1762e5ac
Amaari <arish2k3@gmail.com>
2023-04-28 13:33:57 +0530

    update

--- learn_c_the_hard_way/ex3.c
+++ learn_c_the_hard_way/ex3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	int age = 10;
+	int age;
 	int height = 72;
 
-	printf("I am %d years old.\v", age);
+	printf("I am %d years old.\n", age);
 	printf("I am %d inches tall.\n", height);
 
 	return 0;
