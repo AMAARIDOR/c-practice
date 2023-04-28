d1eae304ce3f7a2054157f740746bf245ded1162
Amaari <arish2k3@gmail.com>
2023-04-28 20:00:11 +0530

    update

--- learn_c_the_hard_way/enum.c
+++ learn_c_the_hard_way/enum.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-enum Level {
-    LOW = 25,
-    MEDIUM = 50,
-    HIGH = 75 
-};
-
-enum Bread {
-    SOUR = 4,
-    NORMAL = 2,
-    CROISSANT = 5
-};
-
 int main() {
-    bool yes = false;
-    enum Level myVar = MEDIUM;
-    enum Bread leastFavoriteBread = NORMAL; 
+    bool yes = true;
 
     if (yes == true) {
         printf("something");
     }
 
-    printf("%d\n", myVar);
-    printf("%d\n", leastFavoriteBread);
-
-    return 0;
+    return printf("It failed lol\n");
 }
