059c07b6349662977588ba81a1e7a12958a2aec4
Amaari <arish2k3@gmail.com>
2023-04-30 19:52:26 +0530

    update

--- the_c_programming_lang_book/histogram_visualization.c
+++ the_c_programming_lang_book/histogram_visualization.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
 int main() {
+	int c, tc, tb, tt, tn;
+
+	c = 0; 
+	tc = 0;
+	tb = 0;
+	tt = 0;
+	tn = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c != ' ' || c != '\n' || c != '\t')
+			tc++;
+		else if (c == ' ')
+			tb++;
+		else if (c == '\t')
+			tt++;
+		else if (c == '\n')
+			tn++;
+	}
+
+	putchar('\n');
+
+	for (int i = 0; i < tc; i++) {
+		printf("%s", "-");
+	}
+
+	putchar('\n');
+
+	for (int i = 0; i < tb; i++) {
+		printf("%s", "-");
+	}
+	
+	putchar('\n');
+
+	for (int i = 0; i < tt; i++) {
+		printf("%s", "-");
+	}
+
+	putchar('\n');
+
+	for (int i = 0; i < tn; i++) {
+		printf("%s", "-");
+	}
+	
+	putchar('\n');
 
 	return 0;
 }
