38251d0452dab2eb22d42234ea0a16ad989a6bb9
Amaari <arish2k3@gmail.com>
2023-05-01 18:19:50 +0530

    update

--- the_c_programming_lang_book/char_arrays.c
+++ the_c_programming_lang_book/char_arrays.c
@@ -9,7 +9,6 @@ int main() {
 	int max;
 	char line[MAXLINE];
 	char longest[MAXLINE];
-
 	max = 0;
 	while ((len = getline(line, MAXLINE)) > 0)
 		if (len > max) {
@@ -23,7 +22,6 @@ int main() {
 
 int getline(char s[], int lim) {
 	int c, i;
-
 	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
 		s[i] = c;
 	if (c == '\n') {
@@ -36,7 +34,6 @@ int getline(char s[], int lim) {
 
 void copy(char to[], char from[]) {
 	int i;
-
 	i = 0;
 	while ((to[i] = from[i]) != '\0')
 		++i;
