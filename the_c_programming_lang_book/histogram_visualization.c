d5e7c5eee35ebbdb60ff1eb5f919e0e3ac2c66d5
Amaari <arish2k3@gmail.com>
2023-05-01 14:42:16 +0530

    update

--- the_c_programming_lang_book/histogram_visualization.c
+++ the_c_programming_lang_book/histogram_visualization.c
@@ -1,50 +1,34 @@
 #include <stdio.h>
 
-int main() {
-	int c, tc, tb, tt, tn;
-
-	c = 0; 
-	tc = 0;
-	tb = 0;
-	tt = 0;
-	tn = 0;
-
-	while ((c = getchar()) != EOF) {
-		if (c != ' ' || c != '\n' || c != '\t')
-			tc++;
-		else if (c == ' ')
-			tb++;
-		else if (c == '\t')
-			tt++;
-		else if (c == '\n')
-			tn++;
+static inline
+void histogram(void) {
+	int space = 1;
+	int tab = 1;
+	int newline = 1;
+	int other = 1;
+	while (1) {
+		switch (getchar()) {
+			case EOF:
+				goto histogram_print;
+			case '\t': ++tab; break;
+			case '\n': ++newline; break;
+			case ' ': ++space; break;
+			default: ++other; break;
+		}
 	}
-
+histogram_print:
+	puts("characters:");
+	while (--other) putchar('-');
+	puts("\nspaces:");
+	while (--space) putchar('-');
+	puts("\ntabs:");
+	while (--tab) putchar('-');
+	puts("\nnewlines:");
+	while (--newline) putchar('-');
 	putchar('\n');
+}
 
-	for (int i = 0; i < tc; i++) {
-		printf("%s", "-");
-	}
-
-	putchar('\n');
-
-	for (int i = 0; i < tb; i++) {
-		printf("%s", "-");
-	}
-	
-	putchar('\n');
-
-	for (int i = 0; i < tt; i++) {
-		printf("%s", "-");
-	}
-
-	putchar('\n');
-
-	for (int i = 0; i < tn; i++) {
-		printf("%s", "-");
-	}
-	
-	putchar('\n');
-
+int main(void) {
+	histogram();
 	return 0;
 }
