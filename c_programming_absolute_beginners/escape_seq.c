cf9b220df96740868781d69a820b22fe9dcdc3df
Amaari <arish2k3@gmail.com>
2023-05-02 13:33:59 +0530

    update

---
+++ c_programming_absolute_beginners/escape_seq.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main() {
+    printf("Column A\tColumn B\tColumn C");
+    printf("\nMy Computer\'s Beep Sounds Like this: \a!\n");
+    printf("\"Letz\bs fix that typo and then show the backslash ");
+    printf("character \\\" she said\n");
+
+    return 0;
+}
