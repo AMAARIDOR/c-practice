9e2f661c324dc53ebc93d94be9c5e1f65f802ea9
Amaari <arish2k3@gmail.com>
2023-05-02 13:01:40 +0530

    update

--- the_c_programming_lang_book/print_eighty_character.c
+++ the_c_programming_lang_book/print_eighty_character.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#define MAXLINE 1000
+
+int linegetter(char line[], int maxline);
+void copier(char to[], char from[]);
 
 int main() {
+    int len;
+    int max;
+    char line[MAXLINE];
+
+    max = 0;
+    while ((len = linegetter(line, MAXLINE)) > 0)
 
     return 0;
+}
+
+int linegetter(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
 }
