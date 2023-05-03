ac9fa426f5e7dac7609952eefbaf7747b528a431
Amaari <arish2k3@gmail.com>
2023-05-03 15:16:45 +0530

    update

--- learn_c_the_hard_way/switch_jump_table.c
+++ learn_c_the_hard_way/switch_jump_table.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	int i = 0;
-	int a = 43;
-
-	printf("%p\n", &a);
-
-	// Go through each string in argv
-	for (i = 1; i < argc; i++) {
-		printf("arg %d: %s\n", i, argv[i]);
+	if (argc != 2) {
+		printf("ERROR: You need one argument.\n");
+		return 1;
 	}
 
-	// make our own array of strings
-	
-	char *states[] = {
-		"California", "Oregon", "Washington", "Texas"
-	};
+	int i = 0;
+	for (i = 0; argv[1][i] != '\0'; i++) {
+		char letter = argv[1][i];
 
-	int num_states = 4;
+		switch (letter) {
+			case 'a':
+			case 'A':
+				printf("%d: 'A'\n", i);
+				break;
+			case 'e':
+			case 'E':
+				printf("%d: 'E'\n", i);
+				break;
+			case 'i':
+			case 'I':
+				printf("%d: 'I'\n", i);
+				break;
+			case 'o':
+			case 'O':
+				printf("%d: 'O'\n", i);
+				break;
+			case 'u':
+			case 'U':
+				printf("%d: 'U'\n", i);
+				break;
+			case 'y':
+			case 'Y':
+				if (i > 2) {
+					printf("%d: 'Y'\n", i);
+				}
+				break;
 
-	for (i = 0; i < num_states; i++) {
-		printf("state %d: %s\n", i, states[i]);
+			default:
+				printf("%d: %c is not a vowel\n", i, letter);
+		}
 	}
 
 	return 0;
